3748cc5af91646102c18b8ce290785fb3aece74a
tao <3065913669@qq.com>
2020-09-02 13:56:04 +0800

    add /**/

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # the-c-programming-language
 C 程序设计语言学习记录
+
+很多题目都读不懂，能坚持到那个位置
--- theCProgrammingLanguage/001导言/code/1.6 数组/1-13.c
+++ theCProgrammingLanguage/001导言/code/1.6 数组/1-13.c
@@ -3,8 +3,29 @@
     水平方向的直方图比较容易绘制，垂直方向的直方图则要困难些 */
 
 /* 
-
-*/
+1. 初始化 记录单词字符数的数组
+2. 获取输入，记录单词的长度到指定索引
+    2.1 如果字符是空
+        2.1.1 在单词外，state = OUT
+        2.1.2 如果单词字符数nc > 0
+            2.1.2.1 单词字符数 小于 单个单词最大字符数
+                2.1.2.1.1 nc是单词长度，长度为nc的索引处+1
+            2.1.2.2
+                超出长度 +1
+        nc = 0
+    2.2 不是空
+        2.2.1 state 是 out ，说明上一个字符是空
+            修改state 为In
+            nc = 1
+        2.2.2 state 不是out，说明已经在一个单词里面了
+            nc ++
+3. 长度的最大值 = 0
+4. 遍历wl 获得maxvalue的值
+5. 遍历wl 
+    5.1 打印 各个长度 各个长度的数量
+    5.2 如果该长度的数量 > 0
+        5.2.1 
+*/      
 #include <stdio.h>
 
 #define MAXHIST 15       /* max length of histogram */
@@ -27,6 +48,7 @@ void main () {
     for (i=0;i<MAXWORD;++i) {
         wl[i] = 0;
     }
+
     while ((c = getchar()) != EOF) {
         if (c == ' ' || c == '\n' || c == '\t' ) {
             state = OUT;
@@ -52,10 +74,10 @@ void main () {
         }
     }
     for (i=1;i<MAXWORD;++i) {
-        printf("%5d - %5d : ", i, wl[i]);
+        printf("%5d - %5d:", i, wl[i]);
         if (wl[i] > 0) {
             if ((len = wl[i] * MAXHIST/maxvalue) <= 0) {
-                len =1;
+                len = 1;
             } else {
                 len = 0;
             }
