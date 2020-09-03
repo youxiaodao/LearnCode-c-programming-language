f5358d26f0a2a01ecba047f7c7dcbd27b864b96c
youxiaodao <471313931@qq.com>
2020-09-03 08:25:11 +0800

    add 1-14

---
+++ theCProgrammingLanguage/001导言/code/1.6 数组/1-13-2.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+#define MAXHIST 15
+#define MAXWORD 11
+#define IN      1
+#define OUT     0
+
+/* print vertical histogram */
+void main(){
+    int c, state, i, j, nc;
+    int maxvalue;
+    int ovflow;
+    int wl[MAXWORD];
+    
+    state = OUT;
+    nc = 0;
+    ovflow = 0;
+
+    for (i=0;i<MAXWORD;++i){
+        wl[i] = 0;
+    }
+    while ((c=getchar()) != EOF){
+        if (c == ' ' || c == '\t' || c == '\n'){
+            state = OUT;
+            if (nc > 0) {
+                if (nc < MAXWORD) {
+                    ++wl[nc];
+                } else {
+                    ++ovflow;
+                }
+            }
+            nc = 0;
+        } else if (state == OUT) {
+            state = IN;
+            nc = 1;
+        } else {
+            ++nc;
+        }
+    }
+
+    maxvalue = 0;
+    for (i=1; i<MAXWORD;++i) {
+        if (wl[i] > maxvalue) {
+            maxvalue = wl[i];
+        }
+    }
+
+    for (i=MAXHIST; i > 0; --i) {
+        for (j=1;j< MAXWORD;++j) {
+            if (wl[j] * MAXHIST / maxvalue >= i) {
+                printf("   * ");
+            }else{
+                printf("     ");
+            }
+        }
+        putchar('\n');
+    }
+
+    for (i=1;i<MAXWORD;++i){
+        printf("%4d ", i);
+    }
+    putchar('\n');
+
+    for (i = 1; i < MAXWORD; ++i) {
+        printf("%4d ", wl[i]);
+    }
+    putchar('\n');
+    if (ovflow > 0 ) {
+        printf("There are %d words >= %d\n", ovflow, MAXWORD);
+    }
+}
